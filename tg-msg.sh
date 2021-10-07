b6f5fdf6106cda3a50dfb5800638de3d2aad43c2
migrate-bricks <github-action@github.com>
2021-10-07 23:54:37 +0800

    update tg-msg.sh

--- tg-msg.sh
+++ tg-msg.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 (
-  sleep 5
+  sleep 30
+  #132***2789
   echo "msg @JD_ShareCode_Bot /farm e0e4bc319b924c4ea64cce1b2118dad7"
   echo "msg @JD_ShareCode_Bot /pet MTE5MzEwNTEzODAwMDAwMDA1Mzc2MTI4NQ=="
   echo "msg @JD_ShareCode_Bot /bean aogye6x4cnc3oj4hf75thvmdjxiaky77vxjys4i"
   echo "msg @JD_ShareCode_Bot /ddfactory T0205KkcJkhCpBGtaE6j6q5MCjVWnYaS5kRrbA"
   echo "msg @JD_ShareCode_Bot /sgmh T0205KkcJkhCpBGtaE6j6q5MCjVQmoaT5kRrbA"
   echo "msg @JD_ShareCode_Bot /health T0205KkcJkhCpBGtaE6j6q5MCjVfnoaW5kRrbA"
-  echo "completed.."
+
+  #156***4409
+  echo "msg @JD_ShareCode_Bot /farm 87a7f05dbd6a4564903792c1745695bc"
+  echo "msg @JD_ShareCode_Bot /pet MTE1MzEzNjI2MDAwMDAwMDU2MzY4OTE5"
+  echo "msg @JD_ShareCode_Bot /bean dzfuhp3b2fz7medovlqxspml2k6gxejshui27pq"
+  echo "msg @JD_ShareCode_Bot /ddfactory T020a3LolJe5Iv5j9bpSQW-9CjVWnYaS5kRrbA"
+  echo "msg @JD_ShareCode_Bot /sgmh T020a3LolJe5Iv5j9bpSQW-9CjVQmoaT5kRrbA"
+  echo "msg @JD_ShareCode_Bot /health T020a3LolJe5Iv5j9bpSQW-9CjVfnoaW5kRrbA"
 ) | docker exec -i telegram-cli telegram-cli -N 
