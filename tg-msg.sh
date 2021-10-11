09357208fc694b160f86b993bfb4b4f5f5541d35
migrate-bricks <github-action@github.com>
2021-10-11 23:47:29 +0800

    update add new account share code

--- tg-msg.sh
+++ tg-msg.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 (
-  echo "resolve_username JD_ShareCode_Bot"
-  sleep 5
-  #132***2789
-  echo "msg @JD_ShareCode_Bot /farm e0e4bc319b924c4ea64cce1b2118dad7"
-  echo "msg @JD_ShareCode_Bot /pet MTE5MzEwNTEzODAwMDAwMDA1Mzc2MTI4NQ=="
-  echo "msg @JD_ShareCode_Bot /bean aogye6x4cnc3oj4hf75thvmdjxiaky77vxjys4i"
-  echo "msg @JD_ShareCode_Bot /ddfactory T0205KkcJkhCpBGtaE6j6q5MCjVWnYaS5kRrbA"
-  echo "msg @JD_ShareCode_Bot /sgmh T0205KkcJkhCpBGtaE6j6q5MCjVQmoaT5kRrbA"
-  echo "msg @JD_ShareCode_Bot /health T0205KkcJkhCpBGtaE6j6q5MCjVfnoaW5kRrbA"
-
-  #156***4409
-  echo "msg @JD_ShareCode_Bot /farm 87a7f05dbd6a4564903792c1745695bc"
-  echo "msg @JD_ShareCode_Bot /pet MTE1MzEzNjI2MDAwMDAwMDU2MzY4OTE5"
-  echo "msg @JD_ShareCode_Bot /bean dzfuhp3b2fz7medovlqxspml2k6gxejshui27pq"
-  echo "msg @JD_ShareCode_Bot /ddfactory T020a3LolJe5Iv5j9bpSQW-9CjVWnYaS5kRrbA"
-  echo "msg @JD_ShareCode_Bot /sgmh T020a3LolJe5Iv5j9bpSQW-9CjVQmoaT5kRrbA"
-  echo "msg @JD_ShareCode_Bot /health T020a3LolJe5Iv5j9bpSQW-9CjVfnoaW5kRrbA"
-  echo "safe_quit"
-) | docker exec -i telegram-cli telegram-cli -N 
+    echo "resolve_username JD_ShareCode_Bot"
+    sleep 5
+    #132***2789
+    echo "msg @JD_ShareCode_Bot /farm e0e4bc319b924c4ea64cce1b2118dad7"
+    echo "msg @JD_ShareCode_Bot /pet MTE5MzEwNTEzODAwMDAwMDA1Mzc2MTI4NQ=="
+    echo "msg @JD_ShareCode_Bot /bean aogye6x4cnc3oj4hf75thvmdjxiaky77vxjys4i"
+    echo "msg @JD_ShareCode_Bot /ddfactory T0205KkcJkhCpBGtaE6j6q5MCjVWnYaS5kRrbA"
+    echo "msg @JD_ShareCode_Bot /sgmh T0205KkcJkhCpBGtaE6j6q5MCjVQmoaT5kRrbA"
+    echo "msg @JD_ShareCode_Bot /health T0205KkcJkhCpBGtaE6j6q5MCjVfnoaW5kRrbA"
+    
+    #156***4409
+    echo "msg @JD_ShareCode_Bot /farm 87a7f05dbd6a4564903792c1745695bc"
+    echo "msg @JD_ShareCode_Bot /pet MTE1MzEzNjI2MDAwMDAwMDU2MzY4OTE5"
+    echo "msg @JD_ShareCode_Bot /bean dzfuhp3b2fz7medovlqxspml2k6gxejshui27pq"
+    echo "msg @JD_ShareCode_Bot /ddfactory T020a3LolJe5Iv5j9bpSQW-9CjVWnYaS5kRrbA"
+    echo "msg @JD_ShareCode_Bot /sgmh T020a3LolJe5Iv5j9bpSQW-9CjVQmoaT5kRrbA"
+    echo "msg @JD_ShareCode_Bot /health T020a3LolJe5Iv5j9bpSQW-9CjVfnoaW5kRrbA"
+    
+    #173****8741
+    echo "msg @JD_ShareCode_Bot /farm eebf469beae04250b1e3fa7b599862cc"
+    echo "msg @JD_ShareCode_Bot /pet MTEzMzI1MTE4NDAwMDAwMDA1Mzc2MTI3OQ=="
+    echo "msg @JD_ShareCode_Bot /bean gcdr655xfdjq6g4xh3ddm4qkugf5urltct3sjqa"
+    echo "msg @JD_ShareCode_Bot /ddfactory T0205KkcO1dlkQ2pcUCXzp1RCjVWnYaS5kRrbA"
+    echo "msg @JD_ShareCode_Bot /sgmh T0205KkcO1dlkQ2pcUCXzp1RCjVQmoaT5kRrbA"
+    echo "msg @JD_ShareCode_Bot /health T0205KkcO1dlkQ2pcUCXzp1RCjVfnoaW5kRrbA"
+    echo "safe_quit"
+) | docker exec -i telegram-cli telegram-cli -N
