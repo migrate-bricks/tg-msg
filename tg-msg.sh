17073639b961befe8d6377b7fe8b6747840f02e7
migrate-bricks <github-action@github.com>
2021-10-16 18:22:20 +0800

    upload share code

--- tg-msg.sh
+++ tg-msg.sh
@@ -2,7 +2,16 @@
 (
     echo "resolve_username JD_ShareCode_Bot"
     sleep 5
-    #132***2789
+    #132***2789 jd_5006e4d4017b2
+	echo "msg @JD_ShareCode_Bot /farm 12fc0bf246ac4a28b51d89de2fa288ec"
+	echo "msg @JD_ShareCode_Bot /pet MTEyNjkzMjAwMDAwMDAwMDUzMzc2ODA3"
+	echo "msg @JD_ShareCode_Bot /bean mlrdw3aw26j3x3y3tsw6bzph53cddhzxm6apgxy"
+	echo "msg @JD_ShareCode_Bot /ddfactory T0225KkcRR4Z8wHTdB70lfBYdgCjVWnYaS5kRrbA"
+	echo "msg @JD_ShareCode_Bot /jxfactory sh4SPz52VpAT5iJSlQ0mcg=="
+	echo "msg @JD_ShareCode_Bot /sgmh T0225KkcRR4Z8wHTdB70lfBYdgCjVQmoaT5kRrbA"
+	echo "msg @JD_ShareCode_Bot /health T0225KkcRR4Z8wHTdB70lfBYdgCjVfnoaW5kRrbA"
+
+	#135***4960 jd_VfkauJxdgNiv
     echo "msg @JD_ShareCode_Bot /farm e0e4bc319b924c4ea64cce1b2118dad7"
     echo "msg @JD_ShareCode_Bot /pet MTE5MzEwNTEzODAwMDAwMDA1Mzc2MTI4NQ=="
     echo "msg @JD_ShareCode_Bot /bean aogye6x4cnc3oj4hf75thvmdjxiaky77vxjys4i"
@@ -10,7 +19,7 @@
     echo "msg @JD_ShareCode_Bot /sgmh T0205KkcJkhCpBGtaE6j6q5MCjVQmoaT5kRrbA"
     echo "msg @JD_ShareCode_Bot /health T0205KkcJkhCpBGtaE6j6q5MCjVfnoaW5kRrbA"
     
-    #156***4409
+    #156***4409 快乐的吖娇
     echo "msg @JD_ShareCode_Bot /farm 87a7f05dbd6a4564903792c1745695bc"
     echo "msg @JD_ShareCode_Bot /pet MTE1MzEzNjI2MDAwMDAwMDU2MzY4OTE5"
     echo "msg @JD_ShareCode_Bot /bean dzfuhp3b2fz7medovlqxspml2k6gxejshui27pq"
@@ -18,7 +27,7 @@
     echo "msg @JD_ShareCode_Bot /sgmh T020a3LolJe5Iv5j9bpSQW-9CjVQmoaT5kRrbA"
     echo "msg @JD_ShareCode_Bot /health T020a3LolJe5Iv5j9bpSQW-9CjVfnoaW5kRrbA"
     
-    #173****8741
+    #173****8741 jd_KyLTiNajSjZk
     echo "msg @JD_ShareCode_Bot /farm eebf469beae04250b1e3fa7b599862cc"
     echo "msg @JD_ShareCode_Bot /pet MTEzMzI1MTE4NDAwMDAwMDA1Mzc2MTI3OQ=="
     echo "msg @JD_ShareCode_Bot /bean gcdr655xfdjq6g4xh3ddm4qkugf5urltct3sjqa"
